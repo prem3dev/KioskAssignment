d61016e570d11b532270ccba88fe95f97f313167
prem3dev <ecstasy1508@gmail.com>
2025-03-10 10:21:38 +0900

    inpputNumber 후 출력 수정

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package kioskassignment;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,9 +22,11 @@ public class Main {
         menuItemList.add(new MenuItem("Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
 
         //반복문을 통해 리스트 전체 출력
-        for(int i = 0; i < menuItemList.size(); i++) {
+        int index = 0;
+        for(MenuItem d : menuItemList) {
+            ++index;
             System.out.println("--------------------");
-            System.out.println(menuItemList.get(i));
+            System.out.println(index + "." + " " + d.getHanmbergerName() + " " + d.getHambergerPrice() + " " + d.getHambergerContent());
         }
 
         //입력번호를 이용하여 선택 메뉴 출력
@@ -31,7 +34,9 @@ public class Main {
         System.out.println("원하시는 메뉴의 번호를 입력하세요");
         try {
             int inputNumber = scanner.nextInt();
-            System.out.println(menuItemList.get(inputNumber) + "를 선택하셨습니다.");
+            System.out.println((menuItemList.get(inputNumber-1).getHanmbergerName()) + "를 선택하셨습니다.\n" +
+                    "결제금액은 " + menuItemList.get(inputNumber-1).getHambergerPrice() + "원 입니다.\n" +
+                    "감사합니다. 좋은 하루 되세요");
         } catch (Exception e) {
             System.out.println("잘못된 번호입니다.");
         }
